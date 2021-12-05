8a787cc02c84ecec01c7bfa4ba8853023e3b4f57
NateKnob <nate.knauf@gmail.com>
2021-12-05 14:21:13 -0500

    Restructure Streamlit

--- streamLit.py
+++ streamLit.py
@@ -14,37 +14,33 @@ import random
 
 softmax = torch.nn.Softmax(dim = 1)
 st.set_page_config(page_title='ImageNet performance degradation')
+col1, col2 = st.columns(2)
 
-modelName = st.sidebar.radio("Model", ['ResNet152', 'Alexnet', 'VGG16'])
-    
-transformation = st.sidebar.selectbox("Transformation", ['Shuffle', 'Rotate by 45', 'Vertical Shift', 'Horizontal shift'])
-
-imageNetClass = st.sidebar.selectbox("ImageNet Class", ["Teapot", "Wolf", "Cat", "Bulldog"])
-
-# print(modelName, transformation, imageNetClass)
-
-imageNames = {"Bulldog": ['ILSVRC2012_val_00027433'], 
+imageNames = {"Bulldog": ['ILSVRC2012_val_00027433'],
               "Cat": ['ILSVRC2012_val_00045239'],
              "Wolf": ['ILSVRC2012_val_00001172'],
              "Teapot": ['ILSVRC2012_val_00006001']}
 
 imageNetClassIdx = {"Bulldog": 245, "Cat": 281, "Wolf": 269, 'Teapot': 849}
 
-transformationToFolder = {'None': "original", 
+transformationToFolder = {'None': "original",
                           "Shuffle": "shuffle_quad",
                           'Rotate by 45': "rotate",
                           'Vertical Shift':"vert",
-                          'Horizontal shift':"horizontal", 
+                          'Horizontal shift':"horizontal",
                           'Hue + 30': 'hueP30'}
 
-if(modelName == "ResNet152"):
-    model = resnet152(pretrained = True)
-elif(modelName == "VGG16"):
-    model = vgg16(pretrained = True)
-elif(modelName == "Alexnet"):
-    model = alexnet(pretrained = True)
-    
-model.eval()
+imageNetClass = st.sidebar.selectbox("ImageNet Class", ["Teapot", "Wolf", "Cat", "Bulldog"])
+
+transformation = st.sidebar.selectbox("Transformation", ['Shuffle', 'Rotate by 45', 'Vertical Shift', 'Horizontal shift'])
+
+st.sidebar.write('Run Models:')
+model_options = ['Alexnet', 'ResNet152', 'VGG16']
+model_option_checkboxes = []
+for model_option in model_options:
+    model_option_checkboxes.append(st.sidebar.checkbox(model_option))
+open_models = [model_options[i] for i, checked in enumerate(model_option_checkboxes) if checked]
+
 imgName = random.choice(imageNames[imageNetClass]) + ".JPEG"
 image_path = os.path.join(os.path.join("./streamLitHelp/images", transformationToFolder["None"]), imgName)
 groundTruthClass = imageNetClassIdx[imageNetClass]
@@ -59,27 +55,65 @@ preprocessing = transforms.Compose([
 
 img = Image.open(image_path).convert('RGB')
 img_processed = preprocessing(img).unsqueeze(dim = 0)
-out = softmax(model(img_processed)).detach().numpy()[0]
-probClass = out[groundTruthClass]
-if(probClass<0.001):
-    probClass = "{:.3e}".format(probClass)
-else:
-    probClass = "{:.4f}".format(probClass)
-st.image(img, caption='Original Image', width = 300)
-st.markdown("{}'s Probability of {} = {}".format(modelName, imageNetClass, probClass))
 
 imageT_path = os.path.join(os.path.join("./streamLitHelp/images", transformationToFolder[transformation]), imgName)
-
-
 imgT = Image.open(imageT_path).convert('RGB')
 imgT_processed = preprocessing(imgT).unsqueeze(dim = 0)
-outT = softmax(model(imgT_processed)).detach().numpy()[0]
-probTClass = outT[groundTruthClass]
 
-st.image(imgT, caption='Transformed Image', width = 300)
-if(probTClass<0.001):
-    probTClass = "{:.3e}".format(probTClass)
-else:
-    probTClass = "{:.4f}".format(probTClass)
-st.markdown("{}'s Probability of transformed {} = {}".format(modelName, imageNetClass, probTClass))
-                          
+@st.cache
+def computeScore(model_name, target_img):
+    if(model_name == "ResNet152"):
+        model = resnet152(pretrained = True)
+    elif(model_name == "VGG16"):
+        model = vgg16(pretrained = True)
+    elif(model_name == "Alexnet"):
+        model = alexnet(pretrained = True)
+
+    model.eval()
+    return model(target_img)
+
+def renderImages():
+    col1.header("Original")
+    col1.image(img, caption=(imageNetClass + ", Original"), width = 300)
+    col2.header("Transformed")
+    col2.image(imgT, caption=(imageNetClass + ", " + transformation), width = 300)
+
+    scores = []
+    scoresT = []
+
+    for model_name in model_options:
+        probClass = np.nan
+        probTClass = np.nan
+        if model_name in open_models:
+            out = softmax(computeScore(model_name, img_processed)).detach().numpy()[0]
+            outT = softmax(computeScore(model_name, imgT_processed)).detach().numpy()[0]
+            probClass = out[groundTruthClass]
+            probTClass = outT[groundTruthClass]
+            # if(probClass<0.001):
+            #     probClass = "{:.3e}".format(probClass)
+            # else:
+            #     probClass = "{:.4f}".format(probClass)
+            # if(probTClass<0.001):
+            #     probTClass = "{:.3e}".format(probTClass)
+            # else:
+            #     probTClass = "{:.4f}".format(probTClass)
+        scores.append(probClass)
+        scoresT.append(probTClass)
+    P_label = "P({})".format(imageNetClass)
+    with col1:
+        st.table(pd.DataFrame(
+            data=scores,
+            index=model_options,
+            columns=[P_label]
+        ).style.format({P_label: '{:.4f}'}))
+    with col2:
+        st.table(pd.DataFrame(
+            data=np.asarray([scoresT, 1-np.divide(scoresT, scores)]).T,
+            index=model_options,
+            columns=[P_label, "Damage"]
+        ).style.format({P_label: '{:.4f}', "Damage": '{:.4f}'}))
+
+
+# print(modelName, transformation, imageNetClass)
+renderImages()
+# renderScores()
